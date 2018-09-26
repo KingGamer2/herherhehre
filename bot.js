81bad6891a78b42e455ba8eb60dd6d91e4bef913
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-09-26 07:19:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const moment = require('moment');
 const Util = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
